956c4d8dce381715e64fefb98284cb1c55cd529f
Aman Singh <amansingh08087@gmail.com>
2026-02-16 21:11:12 +0530

    search_feature

---
+++ lib/features/search/pages/search_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SearchScreen extends StatelessWidget {
+  const SearchScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child:Padding(padding:EdgeInsets.only(top:20),child:Column(
+        children: [],
+      )),
+    ));
+  }
+}
